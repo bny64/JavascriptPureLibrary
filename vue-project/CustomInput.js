e0e0adce01478c4982f8cbb6fda7188ddebcdcd0
namyul <bny64@naver.com>
2026-03-09 08:03:15 +0900

    [Add] Vue.js와 Vee-Validate를 이용한 동적 폼 검증 예제 추가 (CustomInput 및 useFieldArray 사용)

--- vue-project/CustomInput.js
+++ vue-project/CustomInput.js
@@ -1,47 +1,54 @@
+/**
+ * CustomInput
+ * - VeeValidate useField 기반 입력 컴포넌트
+ * - blur 시에만 유효성 검증
+ * - 에러/값 초기화는 부모의 useForm > resetForm() 으로 처리
+ */
 const CustomInput = {
-    props: ['modelValue', 'name', 'type', 'rules', 'placeholder'],
+    props: {
+        modelValue: { default: '' },
+        name: { type: String, required: true },
+        type: { type: String, default: 'text' },
+        rules: { type: Function, default: null },
+        placeholder: { type: String, default: '' },
+    },
+    emits: ['update:modelValue'],
     template: `
-        <div>
-            <input 
+        <div class="custom-input-wrapper">
+            <input
                 :type="type"
-                v-model="value"
+                :value="value"
                 :class="['custom-input', { 'is-invalid': errorMessage }]"
-                @blur="onCustomBlur"
+                @input="onInput"
+                @blur="onBlur"
                 :placeholder="placeholder"
-            >
+            />
             <span v-if="errorMessage" class="error-msg">{{ errorMessage }}</span>
         </div>
     `,
-    setup(props) {
-        // 전역 스코프 충돌 방지를 위해 컴포넌트 내부에서 참조
+    setup(props, { emit }) {
         const { useField } = VeeValidate;
 
-        // useField를 사용하여 상위 useForm 컨텍스트에 필드를 등록합니다.
-        // syncVModel: true 옵션을 사용하여 v-model(modelValue)과 자동으로 동기화합니다.
-        const { value, errorMessage, validate } = useField(() => props.name, props.rules, {
-            initialValue: props.modelValue,
-            syncVModel: true
-        });
-
-        // 사용자가 아무것도 입력하지 않고 단순히 포커스만 잃었을 때도
-        // 강제로 검증을 실행하여 에러 메시지를 노출합니다.
-        const onCustomBlur = async (event) => {
-            // 삭제 버튼이나 초기화 버튼을 눌러서 포커스가 이동하는 경우 검증 생략 (깜빡임 방지)
-            const target = event.relatedTarget;
-            if (target && (
-                target.classList.contains('btn-remove') ||
-                target.classList.contains('btn-reset') ||
-                target.classList.contains('btn-clear')
-            )) {
-                return;
+        const { value, errorMessage, handleBlur, handleChange, validate } = useField(
+            () => props.name,
+            props.rules,
+            {
+                initialValue: props.modelValue ?? '',
+                validateOnValueUpdate: false,
             }
-            await validate();
+        );
+
+        const onInput = (e) => {
+            const newVal = e.target.value;
+            handleChange(newVal, false);
+            emit('update:modelValue', newVal);
         };
 
-        return {
-            value,
-            errorMessage,
-            onCustomBlur
+        const onBlur = async (e) => {
+            handleBlur(e);
+            if (props.rules) await validate();
         };
+
+        return { value, errorMessage, onInput, onBlur };
     }
 };
