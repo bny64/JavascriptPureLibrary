a8f8f6c9d57c0ce6ab41d8724834042cf7e647c8
namyul <bny64@naver.com>
2026-03-11 20:52:45 +0900

    [Update] 상품 가격 입력 필드 추가 및 콤마 자동 포맷팅 기능 구현

--- vue-project/CustomInput.js
+++ vue-project/CustomInput.js
@@ -1,10 +1,11 @@
 const CustomInput = {
-    props: ['modelValue', 'name', 'type', 'rules', 'placeholder'],
+    props: ['modelValue', 'name', 'type', 'rules', 'placeholder', 'isPrice'],
     template: `
         <div>
             <input 
-                :type="type"
-                v-model="value"
+                :type="computedType"
+                :value="displayValue"
+                @input="handleInput"
                 :class="['custom-input', { 'is-invalid': errorMessage }]"
                 @blur="onCustomBlur"
                 :placeholder="placeholder"
@@ -13,20 +14,40 @@ const CustomInput = {
         </div>
     `,
     setup(props) {
-        // 전역 스코프 충돌 방지를 위해 컴포넌트 내부에서 참조
         const { useField } = VeeValidate;
+        const { computed } = Vue;
 
         // useField를 사용하여 상위 useForm 컨텍스트에 필드를 등록합니다.
-        // syncVModel: true 옵션을 사용하여 v-model(modelValue)과 자동으로 동기화합니다.
         const { value, errorMessage, validate } = useField(() => props.name, props.rules, {
-            initialValue: props.modelValue,
             syncVModel: true
         });
+
+        // 가격 타입일 경우 콤마 표시를 위해 text 타입으로 처리
+        const computedType = computed(() => props.isPrice ? 'text' : props.type);
+
+        // 표시용 값: 가격일 경우 3자리마다 콤마 추가
+        const displayValue = computed(() => {
+            if (props.isPrice && value.value !== undefined && value.value !== null && value.value !== '') {
+                const num = String(value.value).replace(/[^\d]/g, '');
+                return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+            return value.value;
+        });
+
+        // 입력 처리: 콤마 제거 후 숫자만 내부 값으로 저장
+        const handleInput = (event) => {
+            let val = event.target.value;
+            if (props.isPrice) {
+                const rawValue = val.replace(/[^\d]/g, '');
+                value.value = rawValue === '' ? '' : Number(rawValue);
+            } else {
+                value.value = val;
+            }
+        };
 
         // 사용자가 아무것도 입력하지 않고 단순히 포커스만 잃었을 때도
         // 강제로 검증을 실행하여 에러 메시지를 노출합니다.
         const onCustomBlur = async (event) => {
-            // 삭제 버튼이나 초기화 버튼을 눌러서 포커스가 이동하는 경우 검증 생략 (깜빡임 방지)
             const target = event.relatedTarget;
             if (target && (
                 target.classList.contains('btn-remove') ||
@@ -41,6 +62,9 @@ const CustomInput = {
         return {
             value,
             errorMessage,
+            computedType,
+            displayValue,
+            handleInput,
             onCustomBlur
         };
     }
